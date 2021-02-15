58f006307e8120f3259eb90bf67d504ee24fdd48
Saransh Chopra <74055102+Saransh-cpp@users.noreply.github.com>
2021-02-15 22:14:56 +0530

    Added a timer to splash screen (#21)

--- lib/screens/splashScreen.dart
+++ lib/screens/splashScreen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_tutorial/screens/homeScreen.dart';
 
@@ -7,95 +9,36 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    homePageTimer();
+  }
+
+  homePageTimer() {
+    Timer(Duration(seconds: 3), () async {
+      Navigator.pushReplacement(
+          context, PageRouteBuilder(
+        pageBuilder: (c, a1, a2) => HomeScreen(),
+        transitionsBuilder: (c, anim, a2, child) =>
+            FadeTransition(opacity: anim, child: child),
+        transitionDuration: Duration(milliseconds: 1000),
+      ));  // pushing HomePage()
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Learn Flutter",
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: 35,
-            ),
-            Text(
-              "Lets Learn the Mobile Development",
-              style: TextStyle(
-                fontSize: 15,
-              ),
-            ),
-            Text(
-              "Framework of the Future!!",
-              style: TextStyle(
-                fontSize: 15,
-              ),
-            ),
-            SizedBox(
-              height: 75,
-            ),
-            Image.asset(
+        child: Image.asset(
               "assets/images/flutter_logo.png",
               height: 260,
               width: 260,
             ),
-            SizedBox(
-              height: 155,
-            ),
-            // RaisedButton(
-            //   child: Text(
-            //     "Get Started",
-            //     style: TextStyle(
-            //       fontSize: 18,
-            //     ),
-            //   ),
-            //   textColor: Colors.white,
-            //   onPressed: () {},
-            //   color: Color(0xff055999),
-            //   shape: RoundedRectangleBorder(
-            //     borderRadius: BorderRadius.circular(20),
-            //   ),
-            // )
-            Material(
-              color: Color(0xff055999),
-              clipBehavior: Clip.antiAlias,
-              borderRadius: BorderRadius.all(
-                Radius.circular(40),
-              ),
-              child: InkWell(
-                onTap: () {
-                  Navigator.pushReplacement(context,
-                      PageRouteBuilder(
-                        pageBuilder: (c, a1, a2) => HomeScreen(),
-                        transitionsBuilder: (c, anim, a2, child) =>
-                            FadeTransition(opacity: anim, child: child),
-                        // transitionDuration: Duration(milliseconds: 1000),
-                      )
-                  );
-                },
-                child: Container(
-                  width: 300,
-                  height: 55,
-                  child: Center(
-                      child: Text(
-                        "Get Started",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18),
-                      )),
-                ),
-              ),
-            ),
-          ],
         ),
-      ),
-    );
+      );
   }
 }
