fdaf3a119e1340a8ac0ab15992d668a2ebc96789
Saransh <saransh0701@gmail.com>
2021-02-15 20:30:06 +0530

    Appbar added

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/screens/accounts.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -8,6 +9,49 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.white,
+            appBar: AppBar(
+              elevation: 0,
+              backgroundColor: Colors.white,
+              leading: Builder(
+                builder: (BuildContext context) {
+                  return Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 10, 0, 0),
+                    child: GestureDetector(
+                      onTap: () {
+                        Scaffold.of(context).openDrawer();
+                      },
+                      child: Image.asset(
+                          'assets/icons/drawer.png'
+                      ),
+                    ),
+                  );
+                },
+              ),
+              actions: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 10, 10, 0),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          PageRouteBuilder(
+                            pageBuilder: (c, a1, a2) => Accounts(),
+                            transitionsBuilder: (c, anim, a2, child) =>
+                                FadeTransition(opacity: anim, child: child),
+                            // transitionDuration: Duration(milliseconds: 1000),
+                          )
+                      );
+                    },
+                    child: Image.asset(
+                        'assets/icons/account.png'
+                    ),
+                  ),
+                )
+              ],
+            )
+        )
+    );
   }
 }
