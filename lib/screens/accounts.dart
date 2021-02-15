4631effe30d2df7101b24db00ba12657686ef710
infiniteoverflow <aswingopinathan1871@gmail.com>
2021-02-15 22:21:13 +0530

    Updated Accounts Screen

--- lib/screens/accounts.dart
+++ lib/screens/accounts.dart
@@ -3,6 +3,18 @@ import 'package:flutter/material.dart';
 class Accounts extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back,color: Colors.grey,size: 35,),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
   }
 }
